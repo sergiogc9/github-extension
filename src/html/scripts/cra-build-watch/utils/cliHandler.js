9b7e58afb802b0dcbb21eee9c9f7aa398be223c3
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-03-04 16:05:18 +0100

    Improved watch build in html using a cra-build-watch version compatible with react-scripts v5

---
+++ src/html/scripts/cra-build-watch/utils/cliHandler.js
@@ -0,0 +1,67 @@
+const meow = require('meow');
+
+module.exports = meow(
+	`
+    Usage
+      $ cra-build-watch [options]
+
+    Options
+      -b, --build-path Path to the build folder. Absolute or relative path, if relative will be prefixed with project root folder path.
+
+      --chunk-filename Set the naming you want to use for non-entry chunk files. Accepts webpack placeholders such as [id], [name], [hash]. Directories can be supplied.
+
+      --disable-chunks Disable code-splitting / chunks so that only a single bundle.js file is generated. Only available in react-scripts >= 2.0.0.
+
+      -o, --output-filename Set the name to be used for the output bundle. Accepts webpack placeholders such as [id], [name], [hash]. Directories can be supplied.
+
+      -p, --public-path Public URL.
+
+      --after-rebuild-hook Run a command after each build/rebuild (e.g. 'node ./afterbuild.js')
+
+      --after-initial-build-hook Run a command after each the initial build only (e.g. 'node ./afterbuild.js')
+
+      --react-scripts-version Version of the react-scripts package used in your project i.e 2.0.3. If not given it will be implied from your package.json and if it cannot be implied the major version 2 will be the default.
+
+      -v, --verbose
+
+    Examples
+      $ cra-build-watch -b dist/ -p /assets
+      $ cra-build-watch --chunk-filename './js/[chunkhash].[name]' -o './js/myapp'
+      $ cra-build-watch -b dist/ -p /assets --chunk-filename './js/[name]/[hash].js' -v
+`,
+	{
+		flags: {
+			'build-path': {
+				type: 'string',
+				alias: 'b'
+			},
+			'chunk-filename': {
+				type: 'string'
+			},
+			'disable-chunks': {
+				type: 'boolean'
+			},
+			'output-filename': {
+				type: 'string',
+				alias: 'o'
+			},
+			'public-path': {
+				type: 'string',
+				alias: 'p'
+			},
+			'react-scripts-version': {
+				type: 'string'
+			},
+			verbose: {
+				type: 'boolean',
+				alias: 'v'
+			},
+			'after-initial-build-hook': {
+				type: 'string'
+			},
+			'after-rebuild-hook': {
+				type: 'string'
+			}
+		}
+	}
+);
---
+++ src/html/scripts/cra-build-watch/utils/index.js
@@ -0,0 +1,39 @@
+const importCwd = require('import-cwd');
+const semver = require('semver');
+const fs = require('fs-extra');
+const path = require('path');
+
+const DEFAULT_VERSION = {
+	major: 2,
+	minor: 1,
+	patch: 2,
+	concatenatedVersion: 212
+};
+
+exports.isEjected = fs.pathExistsSync(path.join(process.cwd(), 'config/paths.js'));
+
+exports.getReactScriptsVersion = function getReactScriptsVersion(cliVersion) {
+	if (cliVersion) {
+		const versions = {
+			major: Number(semver.major(cliVersion)),
+			minor: Number(semver.minor(cliVersion)),
+			patch: Number(semver.patch(cliVersion)),
+			concatenatedVersion: Number(`${semver.major(cliVersion)}${semver.minor(cliVersion)}${semver.patch(cliVersion)}`)
+		};
+		return versions;
+	}
+
+	const reactScriptsPkg = importCwd.silent('react-scripts/package.json');
+	if (!reactScriptsPkg || !reactScriptsPkg.version) {
+		return DEFAULT_VERSION;
+	}
+
+	const { version } = reactScriptsPkg;
+	const versions = {
+		major: Number(semver.major(version)),
+		minor: Number(semver.minor(version)),
+		patch: Number(semver.patch(version)),
+		concatenatedVersion: Number(`${semver.major(version)}${semver.minor(version)}${semver.patch(version)}`)
+	};
+	return versions;
+};
